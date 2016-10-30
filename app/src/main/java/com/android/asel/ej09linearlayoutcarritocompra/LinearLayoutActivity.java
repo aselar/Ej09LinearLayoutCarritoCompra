6ce4c32e30e641d57b187e5871fc37ca7ef3fb4b
Asel Acebedo Romalde <aselromaldemodulo@gmail.com>
2016-10-30 16:29:25 +0100

    Initial commit

---
+++ app/src/main/java/com/android/asel/ej09linearlayoutcarritocompra/LinearLayoutActivity.java
@@ -0,0 +1,13 @@
+package com.android.asel.ej09linearlayoutcarritocompra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinearLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_layout);
+    }
+}
